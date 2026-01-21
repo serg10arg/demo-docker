76540727828b9033362c50917a053320fc63a5b3
serg10arg <cabrerasergio094@gmail.com>
2026-01-21 15:30:30 -0300

    feat(spotify-plugin): implementar proyecto base con dockerfile-maven-plugin
    Configura la estructura inicial del proyecto Spring Boot, incluyendo el
    Dockerfile y la configuración del plugin de Spotify en el pom.xml para
    la construcción de imágenes Docker.

---
+++ container-builder-tools/spotify-plugin/src/main/java/com/docker/spotifyplugin/SpotifyPluginApplication.java
@@ -0,0 +1,13 @@
+package com.docker.spotifyplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyPluginApplication.class, args);
+    }
+
+}
---
+++ container-builder-tools/spotify-plugin/src/test/java/com/docker/spotifyplugin/SpotifyPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.spotifyplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
