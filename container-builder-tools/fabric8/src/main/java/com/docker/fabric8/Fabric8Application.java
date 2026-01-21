c257295e8f446a03fdf897f5d9d33d100c034235
serg10arg <cabrerasergio094@gmail.com>
2026-01-21 17:33:57 -0300

    feat(fabric8): implementar proyecto base con docker-maven-plugin
    Inicializa la estructura del proyecto Spring Boot y configura el plugin de Fabric8 en el pom.xml
    para la construcción de imágenes de contenedor.

---
+++ container-builder-tools/fabric8/src/main/java/com/docker/fabric8/Fabric8Application.java
@@ -0,0 +1,13 @@
+package com.docker.fabric8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fabric8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fabric8Application.class, args);
+	}
+
+}
---
+++ container-builder-tools/fabric8/src/test/java/com/docker/fabric8/Fabric8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.fabric8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fabric8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
